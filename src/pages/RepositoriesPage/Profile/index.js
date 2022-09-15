48b4140803157b7ba7f05f3933015d353bfe8967
Jessica de Oliveira <jessica-lm-@hotmail.com>
2022-09-15 09:41:26 -0300

    user profile

--- src/pages/RepositoriesPage/Profile/index.js
+++ src/pages/RepositoriesPage/Profile/index.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
 import { MdGroup, MdLocationCity, MdWork, MdLink } from 'react-icons/md';
 import { Container, Header, Avatar, Login, Name, Inner, Data } from './styles';
 
-const Profile = () => (
+const Profile = ({ user }) => (
     <Container>
         <Header>
-            <Avatar src='https://avatars.githubusercontent.com/u/87579128?v=4' />
-            <Login>OliveiraJess</Login>
-            <Name>Jéssica de Oliveira</Name>
+            <Avatar src={user.avatar_url} />
+            <Login>{user.login}</Login>
+            <Name>{user.name}</Name>
         </Header>
         <Inner>
-            <Data><MdGroup size={20} />10&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;30&nbsp;<i>seguindo</i></Data>
-            <Data><MdWork size={20} />Desenvolvedora FrontEnd Jr.</Data>
-            <Data><MdLocationCity size={20} />Orleans</Data>
-            <Data>
-                <MdLink size={20} />
-                <a href='https://www.linkedin.com/in/jessica-de-oliveira/'>LinkedIn</a>
-            </Data>
+            <Data><MdGroup size={20} />{user.following}&nbsp;<i>seguidores</i>&nbsp;&middot;&nbsp;{user.followers}&nbsp;<i>seguindo</i></Data>
+            {user.company && (<Data><MdWork size={20} />{user.company}</Data>)}
+            {user.location && (<Data><MdLocationCity size={20} />{user.location}</Data>)}
+            {user.blog && (<Data><MdLink size={20} /><a href={`\\${user.blog}`}>{user.blog}</a></Data>)}
         </Inner>
     </Container>
 );
+
+Profile.propTypes = {
+    user: PropTypes.shape({
+        login: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        avatar_url: PropTypes.string.isRequired,
+        followers: PropTypes.number.isRequired,
+        following: PropTypes.number.isRequired,
+        company: PropTypes.string,
+        blog: PropTypes.string,
+        location: PropTypes.string,
+    }).isRequired,
+};
 
 export default Profile;
--- src/pages/RepositoriesPage/index.js
+++ src/pages/RepositoriesPage/index.js
@@ -4,17 +4,29 @@ import Profile from "./Profile";
 import Filter from "./Filter";
 import Repositories from "./Repositories";
 
-const RepositoriesPage = () => (
+const RepositoriesPage = () => {
+    const user = {
+        login: 'OliveiraJess',
+        name: 'Jéssica de Oliveira',
+        avatar_url: 'https://avatars.githubusercontent.com/u/87579128?v=4',
+        followers: 30,
+        following: 10,
+        company: null,
+        blog: '',
+        location: 'Orleans - SC'
+    }
 
-    <Container>
-        <Sidebar>
-            <Profile />
-            <Filter />
-        </Sidebar>
-        <Main>
-            <Repositories />
-        </Main>
-    </Container>
-)
+    return (
+        <Container>
+            <Sidebar>
+                <Profile user={user} />
+                <Filter />
+            </Sidebar>
+            <Main>
+                <Repositories />
+            </Main>
+        </Container>
+    )
+};
 
 export default RepositoriesPage;
